9076b68208afd327390360e74b9ba6bbd805f638
Dan <98132259+Dan090807@users.noreply.github.com>
2023-03-05 18:32:28 -0600

    Added methods for Maps

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,6 +10,9 @@ public class App {
         System.out.println("*****************************\n");
 
         Scanner inputScanner = new Scanner(System.in);
+        Scanner inputScanner2 = new Scanner(System.in);
+        Scanner inputScanner3 = new Scanner(System.in);
+        Scanner inputScanner4 = new Scanner(System.in);
 
         System.out.println("Elige el tipo de MAP que se implemetará: ");
         System.out.println("1. HashMap.");
@@ -15,16 +20,77 @@ public class App {
         System.out.println("3. LinkedHashMap.\n");
         
         System.out.print("Tu opción: ");
-        int mapString = inputScanner.nextInt();
+        int mapInt = inputScanner.nextInt();
         
-        HashFactory HashTable = new HashFactory(mapString);
+        HashFactory HashTable = new HashFactory(mapInt);
 
-        HashTable.createMap(mapString);
+        Map<String, ArrayList<String>> MyMap = HashTable.createMap(mapInt);
+
+        ArrayList<String> arrayList = new ArrayList<>();
         
         boolean key = true;
         
+        MapAdmin Admin = new MapAdmin(MyMap);
+
         while(key) {
-            System.out.println("Ingresa el ");
+            System.out.println("1. Agregar un producto.");
+            System.out.println("2. Mostrar categoría.");
+            System.out.println("3. Mostrar datos de un producto.");
+            System.out.println("4. Mostrar datos de un producto ordenados.");
+            System.out.println("5. Mostrar todos los productos y categorías.");
+            System.out.println("6. Mostrar todos los productos y categorías ordenados. \n");
+
+            System.out.print("Tu elección es: ");
+            int optInt = inputScanner.nextInt();
+            System.out.println("");
+            
+            switch (optInt) {
+                case 1: 
+                    System.out.print("Ingresa la categoría en la que quieres añadir objetos: ");
+                    String cla = inputScanner2.nextLine();
+                    System.out.println("");
+
+                    System.out.print("Ingresa el objeto que vas a añadir a esa categoría: ");
+                    String item = inputScanner3.nextLine();
+                    System.out.println("");
+
+                    Admin.add(MyMap, cla, arrayList, item);
+                    
+                    break;
+
+                case 2: 
+                    System.out.print("Ingresa el objeto sobre el que quieres saber su categoría: ");
+                    String cat = inputScanner4.nextLine();
+                    System.out.println("");
+                    
+                    boolean crazy = true;
+
+                    for (String llave : MyMap.keySet()) {
+
+                        for (String product : MyMap.get(llave)) {
+                            
+                            if (cat.equals(product)) {
+                                System.out.println("El objeto " + cat + " se encuentra en la categoría " + llave + "\n");
+
+                                crazy = false;
+
+                                break;
+
+                            } else {
+                                continue;
+                                
+                            }
+                        }
+                    }
+                    
+                    if(crazy == true) {
+                        System.out.println("El producto no existe. \n");
+                        
+                    }
+                break;
+
+                case 3: 
+            }
         }
         inputScanner.close();
     }
--- src/MapAdmin.java
+++ src/MapAdmin.java
@@ -7,7 +7,50 @@ public class MapAdmin {
 
     }
 
-    public void add(String K, String V) {
-        
+    public void add(Map<String, ArrayList<String>> m, String K, ArrayList<String> V, String S) throws Exception{
+
+        try {
+            if(m.get(K) == null) {
+                V.add(S);
+                m.put(K, V);
+            
+            } else {
+                m.get(K).add(S);
+
+            }
+    
+            System.out.println(V + " ha sido ingresado dentro de la categoría " + K + "\n");
+            
+        } catch (Exception e) {
+            System.out.println("Ingresa un dato válido. \n");
+
+        }
+    }
+
+
+    public void remove(Map<String, ArrayList<String>> m, String K, ArrayList<String> V, String S) throws Exception{
+
+        try {
+            if(m.get(K) == null) {
+                System.out.println("No hay nada dentro de la categoría " + K + "\n");
+
+            } else {
+                int index = V.indexOf(S);
+                m.get(K).remove(index);
+                
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Ingresa un dato válido. \n");
+
+        }
+    }
+
+    public String showMap(Map<String, ArrayList<String>> m, String K, ArrayList<String> V, String S) {
+        for (String string : V) {
+            System.out.println(string + ":" + m.get(K));
+        }
+
+        return null;
     }
 }
